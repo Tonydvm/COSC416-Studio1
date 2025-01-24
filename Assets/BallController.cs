44bbc27fbcc964fef69fc82e70719a805fbcac34
Antonio Vazquez-Mackay <73569624+Tonydvm@users.noreply.github.com>
2025-01-24 10:52:32 -0800

    added bonus feature

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,10 +4,27 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField] private float ballSpeed = 2f;
+    [SerializeField] private float jumpForce = 5f;
+    private bool isGrounded = true;
 
     public void MoveBall(Vector2 input)
     {
         Vector3 inputXZPlane = new Vector3(input.x, 0, input.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
+
+        // Jump Logic
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
